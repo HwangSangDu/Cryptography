34f2fb2ee6edb34af6eecc03be649e1dd3c35651
ADAM HWANG <ghkdtkden93@naver.com>
2019-03-26 13:55:49 +0900

    Add biz

---
+++ biz.py
@@ -0,0 +1,32 @@
+def encrypt(plaintext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+        ciphertext += chr(value + 65)
+    return ciphertext
+
+
+def decrypt(ciphertext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 65)
+    return plaintext
+
+
+f = open("./TEXT", 'r')
+plaintext = f.read()
+# print(line)
+f.close()
+
+# print(encrypt(line,"vector"))
+encryptText = encrypt(plaintext,"vector")
+plaintext2 = decrypt(encryptText, "vector")
+print(plaintext2)
+# decrypt("","")
